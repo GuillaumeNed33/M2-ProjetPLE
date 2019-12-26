165661794951d2da8d943f5baf7bd4ef3f454d6b
Guillaume NEDELEC <guillaume.ned33@gmail.com>
2019-12-26 17:48:29 +0100

    functions skeletons

--- src/main/java/bigdata/MainPLE.java
+++ src/main/java/bigdata/MainPLE.java
@@ -15,21 +15,33 @@ public class MainPLE {
 	private static final String JOBS_FILE_URL = "/raw_data/ALCF_repo/jobs.csv";
 	private static final String PATTERNS_FILE_URL = "/raw_data/ALCF_repo/patterns.csv";
 
-	public static void main(String[] args) {
-		if (args.length != 4) {
-			System.out.println("Expected exactly 4 arguments -- " + args.length + " given.");
-		} else {
-			startProgram(args);
-		}
-	}
+	private void getDistribOfDurationNotIDLE() {} // Question 1.a
+	private void getDistribOfDurationIDLE() {} // Question 1.b (pattern = -1)
+	private void getDistribOfDurationAlonePattern(String pattern) {} // Question 1.c (foreach patterns - 22)
+
+	private void getDistribOfNbPatternsPerPhase() {} // Question 2.a
+	private void getDistribOfNbJobsPerPhase() {} // Question 3.a
+
+	private void getDistribOfTotalPFSAccessPerJob() {} // Question 4.a
+	private void getTop10JobsTotalPFSAccess() {} // Question 4.b
+
+	private void getTotalDurationIDLE() {} // Question 5
+
+	private void getDistribOfTotalTimeWithAPatternAlone() {} // Question 6.a
+	private void getTop10patterns() {} // Question 6.b
+
+	private void getLinesMatchingWithPatterns(String[] patterns) {} // Question 7
+
 
 	private static void startProgram(String[] target_patterns) {
 		SparkConf conf = new SparkConf().setAppName("Projet PLE 2019");
 		JavaSparkContext context = new JavaSparkContext(conf);
 
+
 		JavaRDD<String[]> rdd = context.textFile(PHASES_FILE_URL).map(line -> line.split(";"));
 		rdd= rdd.coalesce(5, true); // set number of partitions
 
+		/*** QUESION 7 ***/
 		//Map : key = timestamp_debut-timestamp_fin, value=patterns
 		JavaPairRDD<String, String> rddp = rdd.mapToPair(s -> new Tuple2<String, String>(s[0] + "-" + s[1], s[3]));
 
@@ -51,9 +63,17 @@ public class MainPLE {
 				return matchingPattern;
 			}
 		});
-
 		System.out.println("---------------NOMBRE DE DONNEES: " + rdd.count());
 		System.out.println("---------------NOMBRE DE DONNEES FILTREE: " + filtered.count());
 		System.out.println("---------------NOMBRE DE PARTITIONS : " + rdd.getNumPartitions());
 	}
+
+	public static void main(String[] args) {
+		if (args.length != 4) {
+			System.out.println("Expected exactly 4 arguments -- " + args.length + " given.");
+		} else {
+			startProgram(args);
+		}
+	}
+
 }
