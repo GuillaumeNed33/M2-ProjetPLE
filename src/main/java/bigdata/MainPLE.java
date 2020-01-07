350a54840d821d4cbfd26613fc6db52ea3da88e8
Guillaume NEDELEC <guillaume.nedelec@etu.u-bordeaux.fr>
2020-01-07 19:29:15 +0100

    question 1a

--- src/main/java/bigdata/MainPLE.java
+++ src/main/java/bigdata/MainPLE.java
@@ -1,17 +1,22 @@
 package bigdata;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.DoubleFunction;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.util.StatCounter;
 import scala.Tuple2;
 
 import java.util.Arrays;
 
 public class MainPLE {
 
-	private static final String PHASES_FILE_URL = "/raw_data/ALCF_repo/phases.csv";
+	//private static final String PHASES_FILE_URL = "/user/gnedelec001/phases1Go.csv";
+	private static final String PHASES_FILE_URL = "/user/gnedelec001/phases10Go.csv";
+	//private static final String PHASES_FILE_URL = "/raw_data/ALCF_repo/phases.csv";
 	private static final String JOBS_FILE_URL = "/raw_data/ALCF_repo/jobs.csv";
 	private static final String PATTERNS_FILE_URL = "/raw_data/ALCF_repo/patterns.csv";
 
@@ -21,6 +26,50 @@ public class MainPLE {
 	private static void getDistribOfDurationNotIDLE() {
 		SparkConf conf = new SparkConf().setAppName("Projet PLE 2019");
 		JavaSparkContext context = new JavaSparkContext(conf);
+
+		JavaRDD<String[]> rdd = context.textFile(PHASES_FILE_URL).map(line -> line.split(";"));
+		JavaPairRDD<String, String> rddp = rdd.mapToPair(s -> new Tuple2<String, String>(s[0] + "-" + s[1], s[2] + "-" + s[3]));
+
+		JavaPairRDD<String, String> filtered = rddp.filter(new Function<Tuple2<String, String>, Boolean>() {
+			@Override
+			public Boolean call(Tuple2<String, String> stringTuple) throws Exception {
+				String[] patterns = stringTuple._2().split(",");
+				boolean notIDLE = true;
+				int i = 0;
+				while(notIDLE && i < patterns.length) {
+					String currentPattern = patterns[i];
+					if(currentPattern.equals("-1")) {
+						notIDLE = false;
+					} else {
+						i++;
+					}
+				}
+				return notIDLE;
+			}
+		});
+
+		JavaDoubleRDD doubleForStats = filtered.mapToDouble(new DoubleFunction<Tuple2<String, String>>() {
+			@Override
+			public double call(Tuple2<String, String> stringLongTuple2) throws Exception {
+				String duration = stringLongTuple2._2().split("-")[0];
+				return Double.parseDouble(duration);
+			}
+		});
+
+		StatCounter statCounter = doubleForStats.stats();
+
+		System.out.println("NOMBRE DE DONNEES TOTALE : " + rdd.count());
+        System.out.println("NOMBRE DE DONNEES NON IDLE: " + filtered.count());
+        System.out.println("NOMBRE DE PARTITIONS : " + rdd.getNumPartitions());
+
+        System.out.println("Using StatCounter");
+        System.out.println("Count:    " + statCounter.count());
+        System.out.println("Min:      " + statCounter.min());
+        System.out.println("Max:      " + statCounter.max());
+        System.out.println("Sum:      " + statCounter.sum());
+        System.out.println("Mean:     " + statCounter.mean());
+        System.out.println("Variance: " + statCounter.variance());
+        System.out.println("Stdev:    " + statCounter.stdev());
 	}
 
 	/**
